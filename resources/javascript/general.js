05f8a55e2215dc564663a76be5a8d556e1412eb4
joaquin <jokunero@gmail.com>
2025-11-19 19:03:44 +0100

    terminado el blog

--- resources/javascript/general.js
+++ resources/javascript/general.js
@@ -1,11 +1,29 @@
-//efecto de navegacion 
+const nav = document.querySelector("nav")
+const btnscroll = document.querySelector(".botonscroll")
+const flecha = document.querySelector(".botonscroll i")
+const hrefcroll = document.querySelector("#referenciaflecha")
+
+//efecto de navegacion
 document.addEventListener("scroll",()=>{
-    let nav = document.querySelector("nav")
+    
     if (window.scrollY > 250) {
         nav.style.backgroundColor = "rgba(0, 0, 0,0.7)"
-        nav.style.backdropFilter = "blur(7px)"       
+        nav.style.backdropFilter = "blur(7px)"     
     }else{
         nav.style.backgroundColor = "transparent"
         nav.style.backdropFilter = "none"
     }
+
+    // cambio de direccion
+    if (window.scrollY > ((document.documentElement.scrollHeight/2)-(document.documentElement.scrollHeight/10))) {
+        flecha.classList.remove("bi-arrow-down")
+        flecha.classList.add("bi-arrow-up")
+        hrefcroll.href = "#"
+    }else{
+        flecha.classList.remove("bi-arrow-up")
+        flecha.classList.add("bi-arrow-down")
+        hrefcroll.href = "#referenciafooter"
+    }
 })
+
+

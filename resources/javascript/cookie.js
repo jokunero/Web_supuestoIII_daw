5d59802b9aa04574ee20b6c2d49ef27257c4d562
jokunero <jokunero@gmail.com>
2025-11-25 14:09:14 +0100

    subida de cookies

---
+++ resources/javascript/cookie.js
@@ -0,0 +1,22 @@
+//dom
+const botonaceptar = document.getElementById("aceptarcookie");
+const panel = document.getElementById("bannercookie")
+
+
+//comprovar si existe la variable local , si no , se crea
+let comprovar = localStorage.getItem("panel")
+if (comprovar === null) {
+   localStorage.setItem("panel","true")
+}
+
+botonaceptar.addEventListener("click",()=>{
+    localStorage.setItem("panel","false")
+
+    if (localStorage.getItem("panel") == "false") {
+        panel.style.display = "none"
+    }
+})
+
+if (localStorage.getItem("panel") == "false") {
+    panel.style.display = "none"
+}
